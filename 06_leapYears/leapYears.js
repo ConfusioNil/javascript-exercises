ceeed3d199536bf3abf6b94a06060eb285c023a9
ConfusioNil <60062105+ConfusioNil@users.noreply.github.com>
2023-10-25 14:49:36 +1100

    Add solution to 06 assignment
    Adds solution to determine if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
+    let bool = false;
 
-};
+    if (leapYear % 400 == 0) {
+        bool = true;
+    }
+    else if (leapYear % 100 == 0) {
+        bool = false;
+    }
+    else if (leapYear % 4 == 0) {
+        bool = true;
+    }
+
+    return bool;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
