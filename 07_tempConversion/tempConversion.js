5aba0ef0d1fd1d498e9ec7408ef3fb9fd043db2a
ConfusioNil <60062105+ConfusioNil@users.noreply.github.com>
2023-10-25 14:58:00 +1100

    Add solution to 07 assignment
    Adds the solution to converting temperature for the 07 javascript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
-};
+//formula: (x − 32) × 5/9 °C
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5/9);
 
-const convertToFahrenheit = function() {
-};
+  return celsius;
+}
+
+//formula: (x × 9/5 + 32) °F 
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * (9/5) + 32);
+
+  return fahrenheit;
+}
 
 // Do not edit below this line
 module.exports = {
