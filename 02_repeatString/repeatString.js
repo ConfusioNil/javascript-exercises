eceb81fb3218eaabd3124f43ecf1d71e036b9af1
ConfusioNil <60062105+ConfusioNil@users.noreply.github.com>
2023-10-25 13:31:32 +1100

    02 js exercise solution
    Fixed the npm test and added the solution for 02 assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word , repitition) {
+    if (repitition < 0) return "ERROR";
+    let product = "";
 
-};
+    for (let i = 0; i < repitition; i++) {
+        product += word;
+    }
+
+    return product;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
