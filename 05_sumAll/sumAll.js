e94d4df049667c2a7feffa0a1c3751469fd89247
ConfusioNil <60062105+ConfusioNil@users.noreply.github.com>
2023-10-25 14:43:57 +1100

    Add solution to 05 assignment
    Adds the 05 soltution to the sum of numbers from a to a+n.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(startNum, finalSum) {
+    if (!Number.isInteger(startNum) && !Number.isInteger(finalSum)) return 'ERROR';
 
-};
+    sum = 0;
+
+    for (let i = startNum; i <= finalSum; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
