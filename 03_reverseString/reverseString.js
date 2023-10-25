f1fdc89854f842bb026871f5f7abe21276bad0db
ConfusioNil <60062105+ConfusioNil@users.noreply.github.com>
2023-10-25 13:40:36 +1100

    Add solution to 03 assignment
    Adds the solution to the 03 assignment by reversing the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(reverses) {
+    product = '';
 
-};
+    for (let i = reverses.length -1; i > -1; i--) {
+        product += reverses[i];
+    }
+
+    return product;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
